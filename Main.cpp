e13a8d02ca355ad912357b1ab3943dc70c6c2b8f
Xpliot <80181145+Xpliot@users.noreply.github.com>
2022-03-12 19:07:36 -0500

    adding a main.cpp file

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+// basic C++ code
+int main() {
+   int termOne;
+   int termTwo;
+   cout << "enter two numbers: " << endl;
+   cin  >> termOne;
+   cin  >> termTwo;
+   
+   cout << "num_int = " << termOne << endl;
+   cout << "num_double = " << termTwo << endl;
+   cout << termOne + termTwo << endl;
+
+   return 0;
+}
